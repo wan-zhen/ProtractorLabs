17defc8cee2bca7e030f8699513af3bfa3934935
wanzhen <kelly811124811124@hotmail.com>
2019-03-17 21:08:13 +0800

    登入頁的 e2e

--- e2e/src/user/login.e2e-spec.ts
+++ e2e/src/user/login.e2e-spec.ts
@@ -1,3 +1,30 @@
-describe('the user try to login', () => {
+import { browser, by, element } from "protractor";
 
+describe("the user try to login", () => {
+  it("登入", async () => {
+    await browser.get("/user/login");
+    await element(by.id("userName")).sendKeys("John");
+    await element(by.id("password")).sendKeys("123456");
+    await element(by.buttonText("登入")).click();
+    const url = await browser.getCurrentUrl();
+    console.log(url);
+    expect(url).toContain("events");
+  });
+
+  it("登入錯誤", async () => {
+    await browser.get("/user/login");
+    await element(by.id("userName")).sendKeys("none");
+    await element(by.id("password")).sendKeys("aa");
+    await element(by.buttonText("登入")).click();
+    const errorMsg = await element(by.className("alert-danger")).getText();
+    const hasDom = await element(by.className("alert-danger")).isDisplayed();
+    expect(errorMsg).toBe("錯誤的帳號密碼");
+    expect(hasDom).toBeTruthy();
+  });
+
+  it("按取消回到 event", async () => {
+    await browser.get("/user/login");
+    await element(by.buttonText("取消")).click();
+    expect(await browser.getCurrentUrl()).toContain("events");
+  });
 });
