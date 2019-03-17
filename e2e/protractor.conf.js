e4ffebafdf231f72d315155b6b86e0f4b5fa5804
wanzhen <kelly811124811124@hotmail.com>
2019-03-17 21:05:17 +0800

    設定 protractor 設定檔

--- e2e/protractor.conf.js
+++ e2e/protractor.conf.js
@@ -11,6 +11,8 @@ exports.config = {
   capabilities: {
     browserName: 'chrome',
   },
+  SELENIUM_PROMISE_MANAGER:false,// 停用 controllFlow
+  highlitDelay:2000,// 每個 it 停兩秒 要 run server 才有用
   directConnect: true,
   baseUrl: 'http://localhost:4200/',
   framework: 'jasmine',
@@ -19,10 +21,12 @@ exports.config = {
     defaultTimeoutInterval: 10000,
     print: function() {}
   },
-  onPrepare() {
+  async onPrepare() {
     require('ts-node').register({
       project: require('path').join(__dirname, './tsconfig.e2e.json')
     });
+    // 在不是 angular 環境的設定
+    // await browser.waitForAngularEnabled(false)
     jasmine.getEnv().addReporter(new SpecReporter({ spec: { displayStacktrace: true } }));
   }
 };
