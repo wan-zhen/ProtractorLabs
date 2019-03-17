c0fe32316506444a83da72b495cbd72abc167c1d
wanzhen <kelly811124811124@hotmail.com>
2019-03-17 22:52:42 +0800

    create event e2e 寫到 使用 datepicker 點日期

--- e2e/src/events/create-event.e2e-spec.ts
+++ e2e/src/events/create-event.e2e-spec.ts
@@ -1,3 +1,37 @@
-describe('add a new event', () => {
+import { browser, by, element, $$ } from 'protractor';
 
+fdescribe('add a new event', () => {
+  it('建立活動', async () => {
+    await browser.get('/events/new');
+    await element(by.id('name')).sendKeys('Protractor 實戰');
+    await element(by.className('mat-datepicker-toggle-default-icon')).click();
+    await element(by.tagName('mat-datepicker-content')).isDisplayed();
+    await element(by.className('mat-calendar-period-button')).click();
+    const year = '1997';
+    const month = 'DEC';
+    const day = '31';
+    let i;
+    while (i !== year) {
+      await element(by.className('mat-calendar-previous-button')).click();
+      let findYear = await $$('.mat-calendar-body-cell')
+        .filter(elem => elem.getText().then(text => text === year))
+        .first();
+      if ((await findYear.getText()) === year) {
+        i = year;
+        await findYear.click();
+      }
+    }
+    await $$('.mat-calendar-body-cell')
+      .filter(elem => elem.getText().then(text => text === month))
+      .first()
+      .click();
+    await $$('.mat-calendar-body-cell')
+      .filter(elem => elem.getText().then(text => text === day))
+      .first()
+      .click();
+    expect(element(by.id('eventDate')).getAttribute('value')).toBe(
+      '12/31/1997'
+    );
+    // await browser.sleep(2000);
+  });
 });
