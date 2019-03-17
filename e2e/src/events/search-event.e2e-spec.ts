4f2e75fa7f6c79379e4d856babec82561323b1bb
wanzhen <kelly811124811124@hotmail.com>
2019-03-17 21:09:44 +0800

    搜尋的 e2e ，抓取複雜 dome

--- e2e/src/events/search-event.e2e-spec.ts
+++ e2e/src/events/search-event.e2e-spec.ts
@@ -1,4 +1,28 @@
-describe('search a event', () => {
+import { browser, by, element, $$ } from "protractor";
+fdescribe("search a event", () => {
+  it("搜尋", async () => {
+    await browser.get("/events");
+    await element(by.name("searchTerm")).sendKeys("angular");
+    await element(by.buttonText("搜尋")).click();
+    const title = await element(by.className("modal-title")).getText();
+    expect(title).toBe("符合的議程");
+    // $$('.list-group-item') = element.all(by.className("list-group-item"))
+    const listItem = $$(".list-group-item");
+    const listCount = await listItem.count();
+    expect(listCount).toBe(3);
+    // await listItem.get(1).click();
+    await element(by.linkText("Angular 實戰開發")).click();
+    const url = await browser.getCurrentUrl();
+    expect(url).toContain("events/1");
+    const eventTitle = await element(by.tagName("h2")).getText();
+    expect(eventTitle).toBe("ANGULAR 7 開發實戰：新手入門篇");
+  });
 
+  it("搜尋為空", async () => {
+    await browser.get("/events");
+    await element(by.buttonText("搜尋")).click();
+    const listItem = element.all(by.className("list-group-item"));
+    const listCount = await listItem.count();
+    expect(listCount).toBe(0);
+  });
 });
-
