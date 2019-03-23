e5e1b67ae4c689175bf92ac7143cd2acfb51b5cc
wanzhen <kelly811124811124@hotmail.com>
2019-03-23 12:57:50 +0800

    切換不同 window

--- e2e/src/user/new-user.e2e-spec.ts
+++ e2e/src/user/new-user.e2e-spec.ts
@@ -1,3 +1,23 @@
+import { browser, by, element, $, ExpectedConditions as EC } from 'protractor';
 describe('add a new user', () => {
-
+  it('新增會員', async () => {
+    await browser.get('/user/new');
+    await $('#username').sendKeys('mike');
+    await $('#password').sendKeys('123');
+    await $('#firstName').sendKeys('bob');
+    await $('#lastName').sendKeys('joe');
+    await $('#membershipterm').click();
+    await browser.waitForAngularEnabled(false);
+    const handles = await browser.getAllWindowHandles();
+    await browser.switchTo().window(handles[1]);
+    await browser.executeScript(() => {
+      return window.scrollTo(0, document.body.scrollHeight);
+    });
+    await browser.wait(EC.elementToBeClickable($('#accept')), 5000);
+    await $('#accept').click();
+    await browser.switchTo().window(handles[0]);
+    await browser.waitForAngularEnabled(true);
+    await $('#add').click();
+    expect(await browser.getCurrentUrl()).toBe(browser.baseUrl + 'events');
+  });
 });
