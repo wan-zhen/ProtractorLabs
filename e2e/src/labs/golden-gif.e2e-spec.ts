2d07a485b174725ecea4a4154ece8ba4461d67ed
wanzhen <kelly811124811124@hotmail.com>
2019-03-23 15:02:32 +0800

    截圖測試

--- e2e/src/labs/golden-gif.e2e-spec.ts
+++ e2e/src/labs/golden-gif.e2e-spec.ts
@@ -1,3 +1,19 @@
+import { browser, element, by } from 'protractor';
+import { compareScreenshot, addMask, go, click, see, type } from 'blue-harvest';
 describe('gif page present', () => {
-
+  it('遮住 gif', async () => {
+    await browser.get('/labs/gif');
+    await browser.driver
+      .manage()
+      .window()
+      .setSize(1366, 1024);
+    const golden = `e2e/goldens/giflab.png`;
+    const diffDir = 'e2e/goldens/';
+    const gif_img = element(by.id('gif-img'));
+    await browser.waitForAngular();
+    await addMask(gif_img, 'gray');
+    const actual = await browser.takeScreenshot();
+    const result = await compareScreenshot(actual, golden, diffDir);
+    expect(result).toBeTruthy();
+  });
 });
--- e2e/src/labs/golden.e2e-spec.ts
+++ e2e/src/labs/golden.e2e-spec.ts
@@ -1,6 +1,17 @@
 import { browser, element, by } from 'protractor';
-import * as blueharvest from 'blue-harvest';
-
+import { compareScreenshot, addMask, go, click, see, type } from 'blue-harvest';
 describe('main page present', () => {
-
+  it('截圖', async () => {
+    await browser.get('/');
+    await browser.driver
+      .manage()
+      .window()
+      .setSize(1366, 1024);
+    const golden = 'e2e/goldens/home.png';
+    const diffDir = 'e2e/goldens/'; // 會產生 diff-home.png
+    await browser.waitForAngular(); // 截圖前一定要 wait
+    const actual = await browser.takeScreenshot();
+    const result = await compareScreenshot(actual, golden, diffDir);
+    expect(result).toBeTruthy();
+  });
 });
