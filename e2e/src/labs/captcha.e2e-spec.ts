6132ec5dff3ed5d63e2202b0c79712c392aaf92c
wanzhen <kelly811124811124@hotmail.com>
2019-03-23 13:13:24 +0800

    自定義等待條件

--- e2e/src/labs/captcha.e2e-spec.ts
+++ e2e/src/labs/captcha.e2e-spec.ts
@@ -1,3 +1,24 @@
-describe('the user submit a questionnaire', () => {
+import { browser, by, element, $, ExpectedConditions as EC } from 'protractor';
+fdescribe('the user submit a questionnaire', () => {
+  it('輸入驗證碼', async () => {
+    await browser.get('/labs/captcha');
 
+    await browser.wait(waitForCaptcha, 10000);
+    function waitForCaptcha() {
+      return new Promise((resolve, reject) => {
+        const interval = setInterval(() => {
+          element(by.name('captchaCode'))
+            .getAttribute('value')
+            .then(val => {
+              if (val.length === 4) {
+                clearInterval(interval);
+                resolve('ok');
+              }
+            });
+        }, 500);
+      });
+    }
+    await element(by.buttonText('送出')).click();
+    expect(await element(by.tagName('form')).getText()).toContain('驗證碼正確');
+  });
 });
