9ae6056eb52c781e9ca800a47daad15d55e442b0
wanzhen <kelly811124811124@hotmail.com>
2019-03-23 12:57:28 +0800

    switchTo().alert()

--- e2e/src/events/leave-creating-event.e2e-spec.ts
+++ e2e/src/events/leave-creating-event.e2e-spec.ts
@@ -1,3 +1,12 @@
+import { browser, by, element, $, ExpectedConditions as EC } from 'protractor';
 describe('leave add new event page', () => {
-
+  it('按取消', async () => {
+    await browser.get('/events/new');
+    await element(by.buttonText('取消')).click();
+    await browser
+      .switchTo()
+      .alert()
+      .accept();
+    expect(await $('h1').getText()).toContain('活動列表');
+  });
 });
